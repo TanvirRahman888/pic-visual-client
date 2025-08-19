f0564ca9361e87c0e57cf206b758216d999d5643
TanvirRahman888 <tanvirrahman8888@gmail.com>
2025-08-19 23:12:49 +0600

    Design update

--- src/Components/SubTitle/SubTitle.jsx
+++ src/Components/SubTitle/SubTitle.jsx
@@ -4,7 +4,7 @@ const SubTitle = ({title, serviceDetails}) => {
     return (
         <div className="text-center my-8">
             <h2 className="text-3xl">{title}</h2>
-            <hr />
+            <hr className="border-dotted border-1 mx-5 my-5"/>
             <p>{serviceDetails}</p>
         </div>
     );
